61a361be241f32fe80d1061dfe7d6b7590a02e57
mazixsus <mateusz3249@gmail.com>
2023-05-28 17:46:37 +0200

    2 commit

--- server/src/index.js
+++ server/src/index.js
@@ -14,6 +14,6 @@ app.use(router);
 mongo.connect(process.env.MONGO_URL).then(() => {
     console.log("starting on port 8080");
     app.listen("8080");
-    console.log("1");
+    console.log("2");
 });
 
